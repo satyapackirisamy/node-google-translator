c47bc016db91e7e24d977946534c5f8c8fd608c1
Your Name <satya@alchemdigital.com>
2022-07-21 14:59:37 +0530

    Nodemon and Production environment

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const googleTranslate = require('google-translate')(gApi)
 const express = require('express');
 const app = express();
 const port = 8002;
-
+console.log(gApi);
 //app.listen(port, ()=>console.log(`Listening to the port ${port}`));
 app.listen(process.env.PORT, '0.0.0.0', ()=>console.log(`Listening to the port ${port}. Server is up and running!!`))
-var text = "Hello, This is Sathya Prakash from Chennai. I'm learning NLP using Node Js";
+	var text = "Hello, This is Sathya Prakash from Chennai. I'm learning NLP using Node Js";
 console.log("English: >",text)
 googleTranslate.translate(text, 'es', function(err, translation){
 	console.log("Spanish:>", translation.translatedText);
