f31db00d6fa91a128a366ca4fc9c8321338a400f
Your Name <satya@alchemdigital.com>
2022-07-22 14:38:14 +0530

    unwanted route has been removed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const config = require('config')
 const gApi = config.get('google_api_key');
-const googleTranslate = require('google-translate')(process.env.google_api_key)
+const googleTranslate = require('google-translate')(gApi)
 const express = require('express');
 const app = express();
 const port = 8002;
-console.log(gApi);
+console.log(gApi)
 //app.listen(port, ()=>console.log(`Listening to the port ${port}`));
 app.listen(process.env.PORT, '0.0.0.0', ()=>console.log(`Listening to the port ${port}. Server is up and running!!`))
 	var text = "Hello, This is Sathya Prakash from Chennai. I'm learning NLP using Node Js";
@@ -13,6 +13,7 @@ googleTranslate.translate(text, 'es', function(err, translation){
 	console.log("Spanish:>", translation.translatedText);
 });
 
-app.get('/translate', (req, res)=>{
-	res.send("Welcome to translation")
-});
+// app.get('/translate', (req, res)=>{
+// 	res.send("Welcome to translation")
+// });
+
